9251a9ec4bb33700b83834e7395347eb7656ba9c
Healtar <pierre.dzerahovic@gmail.com>
2023-01-29 22:30:11 +0100

    livrable

--- back/controllers/controller.user.js
+++ back/controllers/controller.user.js
@@ -23,26 +23,27 @@ exports.signup = async (req, res) => {
 exports.login = (req, res) => {
 
        User.findOne({ email: req.body.email})
-                 .then((user) => {
-                    if (!user) {
-                      res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
-                    } else {
-                      bcrypt.compare(req.body.password, user.password)
-                            .then(() => res.status(200).json(
-                              {
-                                userId:user._id,
-                                token: jwt.sign(
-                                  { userId: user._id },
-                                  'RANDOM_TOKEN_SECRET',
-                                  { expiresIn: '24h' }
-                                )
-                              }
-                            ))
-                            .catch((error) => res.status(500).json({error}))
-                    }
-                    
-                 })
-                 .catch((error) => res.status(500).json({ error }))
+           .then((user) => {
+             if (!user) 
+             {
+              res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+             } else {
+               bcrypt.compare(req.body.password, user.password)
+                     .then(() => res.status(200).json(
+                       {
+                         userId:user._id,
+                         token: jwt.sign(
+                           { userId: user._id },
+                           'RANDOM_TOKEN_SECRET',
+                           { expiresIn: '24h' }
+                         )
+                       }
+                     ))
+                     .catch((error) => res.status(500).json({error}))
+             }
+            
+           })
+           .catch((error) => res.status(500).json({ error }))
 
  };
 
